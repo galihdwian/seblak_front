ddaf9091f9d83c15f494977e7b891a6f4b81cc90
galihdwian <galihdwia007@gmail.com>
2026-03-27 11:18:08 +0700

    fix basePath

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     loaderFile: "ImageLoader.js",
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || ''
 }
 
 module.exports = withFlowbiteReact(nextConfig)
